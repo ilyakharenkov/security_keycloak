b5ab3b9ae85d85173f613f12dee997e29c116c6e
ilyakharenkov <ilyakharenkov@gmail.com>
2024-11-10 12:18:38 +0300

    init project

---
+++ src/main/java/by/ik/SecurityKeycloakApplication.java
@@ -0,0 +1,12 @@
+package by.ik;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityKeycloakApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityKeycloakApplication.class, args);
+    }
+}
